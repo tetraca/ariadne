038f41e97731895f6667a74a52590f491a205bcf
tetraca <lawdyarbles@gmail.com>
2013-06-14 22:15:46 -0500

    A new method of representing modules which output something

--- src/module.lisp
+++ src/module.lisp
@@ -20,16 +20,20 @@
   "Generic ariadne module call method"
   (let ((path-info (splitted-info env)))
     ;; Respond to requests for this specific module.
-    (when (string= (second path-info) (identifier this))
-      ;; Handle either an API request or a standard request.
-      (cond ((string= (first path-info) "api")
-	     (api-call this env))
 
-	    ((string= (first path-info) (identifier this))
-	     (main-call this))
+    ;; Module API Request
+    (when (and (string= (first path-info) "api")
+	       (string= (second path-info) (identifier this)))
+      (api-call this env))
 
-	    ((string= (first path-info) "admin")
-	     (admin-call this)))
+    ;; Module Standard Request
+    (when (string= (first path-info) (identifier this))
+      ;; We may want to do more with this in the future.
+      (cond ((string= (second path-info) "admin")
+	     (admin-call this env))
+	    
+	    (t
+	     (main-call this env))
 
       (let ((response (call-next this env)))
 	;; Postprocessing
@@ -39,14 +43,19 @@
 (defgeneric api-call (this env)
   (:documentation "API call handler for a middleware application. The API handler mainly just outputs things handled by the main handler from text."))
 (defmethod  api-call ((this <middleware>) env)
-  "The generic API call handler. Key is a value under which the main calls are stored."
-  ;; Let main call fill the environment appropriately
+  "The generic API call handler."
+  ;; Firstly, strip /api from the path-info request.
+  (setf (getf env :path-info) (subseq (getf env :path-info) 0 3))
+
+  ;; Then, let main-call fill the environment appropriately as normal
   ;; And display the result as text. Normally this should not need
   ;; to be overwritten.
-  (main-call this)
+  (main-call this env)
+
+  ;; The results should be placed in the identifier.
   `(200
     (:content-type "text/plain")
-    (,(getf env key))))
+    (,(getf env (identifier this)))))
 
 
 (defgeneric main-call (this env)
