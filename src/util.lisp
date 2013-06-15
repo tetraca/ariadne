753f9e3f0a8d2e108a2ba6906b93760644a4e8bd
tetraca <lawdyarbles@gmail.com>
2013-06-14 21:33:59 -0500

    Added get-post-info

--- src/util.lisp
+++ src/util.lisp
@@ -5,8 +5,11 @@
 (in-package :ariadne)
 
 (defun splitted-info (env)
-  "Split the page URL arguments into separate sections"
+  "Split the page URL arguments into separate sections."
   (split-sequence #\/ (getf env :page-info)))
 
+(defun get-post-info (env)
+  "Returns the submitted POST plist, if it exists."
+  (clack.request:body-parameter (clack.request:make-request env)))
 
 
